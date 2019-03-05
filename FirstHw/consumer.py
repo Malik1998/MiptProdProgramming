e0dfb01e57176d9fd6b208b6810a7d02c5235faf
gazizullin_netcracker <gazizullin.malik19982014@gmail.com>
2019-03-06 02:19:27 +0300

    small fixes

--- FirstHw/consumer.py
+++ FirstHw/consumer.py
@@ -2,8 +2,11 @@
 import pika
 import traceback, sys, time
 import datetime
+import logging
 
-time.sleep(20)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+time.sleep(15)
 
 
 conn_params = pika.ConnectionParameters('queue', 5672)
@@ -13,9 +16,9 @@ channel = connection.channel()
 channel.queue_declare(queue='randomNumbers')
 
 def callback(ch, method, properties, body):
-    print("We received {}".format(body))
+    logging.info("We received {}".format(body))
     st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    print(st)
+    logging.info(st)
 
 channel.basic_consume(callback, queue='randomNumbers', no_ack=True)
 
@@ -27,3 +30,5 @@ while True:
 	except Exception:
 		channel.stop_consuming()
 		traceback.print_exc(file=sys.stdout)
+	finally:
+		channel.stop_consuming()
--- FirstHw/producer.py
+++ FirstHw/producer.py
@@ -3,8 +3,11 @@ import pika
 import time
 import random
 import datetime
+import logging
+import sys
 
-time.sleep(20)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+time.sleep(15)
 
 
 conn_params = pika.ConnectionParameters('queue', 5672)
@@ -14,20 +17,26 @@ channel = connection.channel()
 channel.queue_declare(queue='randomNumbers')
 
 random.seed(42)
-
-while True:
-	randNum = random.randint(1, 255)
-	
-	channel.basic_publish(exchange='', routing_key='randomNumbers', body=str(randNum))
-	print("Number {} sent".format(randNum))
-
-	st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-	print(st)
-	
-	timeSleep = random.randint(1, 5)
-	print("We will sleep for {} :)".format(timeSleep))
-
-	time.sleep(timeSleep)
     
+    
+while True:
+	try:
+		randNum = random.randint(1, 255)
+		
+		channel.basic_publish(exchange='', routing_key='randomNumbers', body=str(randNum))
+		logging.info("Number {} sent".format(randNum))
+		
+		st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+		logging.info(st)
+		
+		timeSleep = random.randint(1, 5)
+		logging.info("We will sleep for {} :)".format(timeSleep))
 
-connection.close()
+		time.sleep(timeSleep)
+	except KeyboardInterrupt:
+		channel.stop_consuming()
+	except Exception:
+		channel.stop_consuming()
+		traceback.print_exc(file=sys.stdout)
+	finally:
+		channel.stop_consuming()
